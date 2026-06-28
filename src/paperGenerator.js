86b37e7a342ebc3e24001a0914134c7686bdff1a
Keisuke NAITO <81.keinaito@gmail.com>
2026-06-28 14:18:57 +0900

    feat: [GREEN] キャンバスにグリッドを描画できる

--- src/paperGenerator.js
+++ src/paperGenerator.js
@@ -1,1 +1,15 @@
-module.exports = {}
+function generateGridLines(width, height, spacing) {
+  const verticalLines = []
+  for (let x = 0; x <= width; x += spacing) {
+    verticalLines.push(x)
+  }
+
+  const horizontalLines = []
+  for (let y = 0; y <= height; y += spacing) {
+    horizontalLines.push(y)
+  }
+
+  return { verticalLines, horizontalLines }
+}
+
+module.exports = { generateGridLines }
