4d3a305a6737511022e3d6c8a8cc401edc9a623c
Keisuke NAITO <81.keinaito@gmail.com>
2026-06-28 14:18:27 +0900

    test: [RED] キャンバスにグリッドを描画できる

--- tests/paperGenerator.test.js
+++ tests/paperGenerator.test.js
@@ -1,5 +1,13 @@
-const {} = require('../src/paperGenerator')
+const { generateGridLines } = require('../src/paperGenerator')
 
-describe('paperGenerator', () => {
-  test.todo('キャンバスにグリッドを描画できる')
+describe('generateGridLines', () => {
+  test('幅300・高さ200・間隔100のとき縦線の座標を返す', () => {
+    const { verticalLines } = generateGridLines(300, 200, 100)
+    expect(verticalLines).toEqual([0, 100, 200, 300])
+  })
+
+  test('幅300・高さ200・間隔100のとき横線の座標を返す', () => {
+    const { horizontalLines } = generateGridLines(300, 200, 100)
+    expect(horizontalLines).toEqual([0, 100, 200])
+  })
 })
